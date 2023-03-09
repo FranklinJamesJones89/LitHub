7f56aa81ea594c58941687ca812ca04b61630e2f
FranklinJamesJones89 <fjones89@icloud.com>
2023-03-09 11:28:27 -0500

    Create explore.html

--- lithubs/urls.py
+++ lithubs/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'lithubs'
 
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('explore', views.explore, name = 'explore')
 ]
--- lithubs/views.py
+++ lithubs/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from . models import Repository
 
 # Create your views here.
 
 def index(request):
     return render(request, 'lithubs/index.html')
+
+def explore(request):
+    repos = Repository.objects.all()
+    context = {'repos': repos}
+    
+    return render(request, 'lithubs/explore.html', context)
