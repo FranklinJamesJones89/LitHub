df3fdfc7cdfc5cb58562753dca451e22d47a5dcb
FranklinJamesJones89 <fjones89@icloud.com>
2023-03-09 10:19:39 -0500

    Create Repository model in models.py and import Users

--- lithubs/models.py
+++ lithubs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Repository(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    title = models.CharField(max_length = 200)
+    synopsis = models.TextField()
+    genre = models.CharField(max_length = 200)
+    form = models.CharfField(max_length = 200)
+
+    class Meta:
+        verbose_name_plural = 'repositories'
+
+    def __str__(self):
+        return self.title
