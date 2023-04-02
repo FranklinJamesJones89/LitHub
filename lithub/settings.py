1ef2f4fe72965e8057648ffddd8d516da6de5354
FranklinJamesJones89 <fjones89@icloud.com>
2023-04-02 13:16:07 -0400

    Test

--- lithub/settings.py
+++ lithub/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 import cloudinary.uploader
 import cloudinary.api
-from .secret_settings import *
+from secret_settings import *
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
