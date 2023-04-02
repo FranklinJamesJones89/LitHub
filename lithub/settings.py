a5eadd30687f7833c95b3570bb9ca1368f5890a7
FranklinJamesJones89 <fjones89@icloud.com>
2023-04-02 14:03:12 -0400

    Modify cloudinary config

--- lithub/settings.py
+++ lithub/settings.py
@@ -16,7 +16,12 @@ import cloudinary.api
 from pathlib import Path
 
 cloudinary.config (
+    cloud_name = 'cloud_name',
+    api_key = 'api_key',
+    api_secret = 'api_secret'
 )
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
