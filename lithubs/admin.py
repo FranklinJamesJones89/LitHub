811e53aea8b9079354ea5da664eadde8cb461e80
FranklinJamesJones89 <fjones89@icloud.com>
2023-04-02 14:44:27 -0400

    Reset all

--- lithubs/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Repository, LikeRepository, Comment
-
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(Repository)
-admin.site.register(LikeRepository)
-admin.site.register(Comment)
